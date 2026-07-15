0fc7b88c40b386f28c42ddbbdd6aa8c6cab7d0cf
Ammad Zohair <ammad.zohair@03interfaces.com>
2026-07-15 18:06:50 +0500

    shake modifier extension

--- core/shared/src/main/java/com/ammad/shared/extensions/ExtModifier.kt
+++ core/shared/src/main/java/com/ammad/shared/extensions/ExtModifier.kt
@@ -1,20 +1,31 @@
 package com.ammad.shared.extensions
 
+import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.pager.PagerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import com.ammad.shared.shake.ShakeController
+import kotlin.math.roundToInt
 
 fun Modifier.slidingLineTransition(pagerState: PagerState, distance: Float) =
     graphicsLayer {
@@ -61,4 +72,44 @@ fun Modifier.simpleVerticalScrollbar(
             cornerRadius = CornerRadius(width.toPx() / 2, width.toPx() / 2)
         )
     }
+}
+
+fun Modifier.shake(shakeController: ShakeController) = composed {
+    shakeController.shakeConfig?.let { shakeConfig ->
+        val shake = remember { Animatable(0f) }
+        LaunchedEffect(shakeController.shakeConfig) {
+            for (i in 0..shakeConfig.iterations) {
+                when (i % 2) {
+                    0 -> shake.animateTo(1f, spring(stiffness = shakeConfig.intensity))
+                    else -> shake.animateTo(-1f, spring(stiffness = shakeConfig.intensity))
+                }
+            }
+            shake.animateTo(0f)
+        }
+
+        this
+            .rotate(shake.value * shakeConfig.rotate)
+            .graphicsLayer {
+                rotationX = shake.value * shakeConfig.rotateX
+                rotationY = shake.value * shakeConfig.rotateY
+            }
+            .scale(
+                scaleX = 1f + (shake.value * shakeConfig.scaleX),
+                scaleY = 1f + (shake.value * shakeConfig.scaleY),
+            )
+            .offset {
+                IntOffset(
+                    (shake.value * shakeConfig.translateX).roundToInt(),
+                    (shake.value * shakeConfig.translateY).roundToInt(),
+                )
+            }
+    } ?: this
+}
+
+@Composable
+fun Modifier.thenIf(
+    condition: Boolean,
+    builder: @Composable Modifier.() -> Modifier
+): Modifier {
+    return if (condition) this.then(builder()) else this
 }
